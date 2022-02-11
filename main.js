2ca2f1f9c45f125952f8d73b7ac4da9a0ff42bd1
Micaella Morales <morales.micaella@gmail.com>
2022-02-11 03:58:36 -0800

    Bring back express and remove unused import

---
+++ main.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.listen(port, function () {
+  console.log("Listening to port " + port);
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("/signup", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import { auth } from "../firebase";
 import AuthContext from "./AuthContext";
